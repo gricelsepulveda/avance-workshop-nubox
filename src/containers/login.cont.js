7d317972e7e780b11a394949759654f28564e702
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2018-06-14 14:36:45 -0400

    inputs name

--- src/containers/login.cont.js
+++ src/containers/login.cont.js
@@ -29,12 +29,12 @@ class Login extends React.Component {
           <h1 className="nbx-h1">Portal de pagos<br/>¡Bienvenido!</h1>
           <Avatar size="md" color="color-5" firstname="gricel" lastname="sepulveda"/>
           <p className="nbx-p">Ingresa tus datos para pagar</p>
-          <form type="post" className="nbx-form-login">
-            <Input placeholder="Rut emisor"/>
-            <Input placeholder="Rut receptor"/>
-            <Input placeholder="Número de factura"/>
+          <form type="post" action="/Login/Login" className="nbx-form-login">
+            <Input placeholder="Rut emisor" name="rut-emisor" id="rut-emisor"/>
+            <Input placeholder="Rut receptor" name="rut-receptor" id="rut-receptor"/>
+            <Input placeholder="Número de factura" name="n-factura" id="n-factura"/>
             <div className="nbx-captcha-container"></div>
-            <Button nbx-alternative>Ingresar</Button>
+            <Button nbx-alternative type="submit">Ingresar</Button>
           </form>
         </Panel>
       </div>
